9b4bc1ab66de912cc7b7d3ae89654f05dab860e5
Riley Marshall <rileyrm@byu.edu>
2023-01-16 02:28:36 -0700

    Adding comments

--- Mission02/Program.cs
+++ Mission02/Program.cs
@@ -6,6 +6,7 @@ namespace Mission02
     {
         static void Main(string[] args)
         {
+            //Importing random class as well as getting the initial number of rolls
             Random rnd = new Random();
             int[] diceArray = new int[11];
             int numberOfRolls = 0;
@@ -18,7 +19,8 @@ namespace Mission02
             Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
             Console.WriteLine("Total number of rolls = " + numberOfRolls + '\n');
 
-
+            // Generating random numbers between 2-12 as dice roll and added up how many times they occer
+            // for each number
             for (int i = 0; i < numberOfRolls; i++)
             {
                 int randomNum = rnd.Next(2, 13);
@@ -26,12 +28,11 @@ namespace Mission02
                 
             }
      
-
+            //Printing out each number and the percent of how many times it was rolled
             for (int k = 0; k < 11; k++)
             {
                 double percentageNumber = (double)diceArray[k] / (double)numberOfRolls;
                 string astrikString = "";
-                //percentageNumber = (double)diceArray[k]/(double)numberOfRolls;
                 double numOfAsteriks = Math.Round(percentageNumber * 100);
 
                 for (int j = 0; j < numOfAsteriks; j++)
